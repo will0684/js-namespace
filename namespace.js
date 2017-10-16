d262306fbd3447c09ddf5ee4b8f4cfd165afc39c
markonnen <jordan.walcaraz@gmail.com>
2017-10-16 12:14:18 -0400

    syntax fix
    fixes to syntax errors

--- namespace.js
+++ namespace.js
@@ -1,23 +1,20 @@
 var WILL0684 = {
   init: function(){
-    console.log("script in WILL0684 called");
     let div = document.createElement("div");
     div.classList.add("box");
-    let text = document.createTextNode("will0684");
-    div.appendChild(text);
+    div.textContent("will0684")
     let boxes = document.getElementsById("boxes");
     boxes.appendChild(div);
 
     div.addEventListener("click", WILL0684.bbcolour);
     div.addEventListener("mouseover", WILL0684.hghlght);
     div.addEventListener("mouseout", WILL0684.hghlght);
-
-    bbcolour: function (ev){
-      ev.target.style.backgroundColor = "blue";
-      ev.target.style.BorderColor = "orange";
-    };
-    hghlght: function (ev){
-      ev.target.classList.toggle("highlight");
-    };
+  },
+  bbcolour: function (ev){
+    ev.target.style.backgroundColor = "blue";
+    ev.target.style.BorderColor = "orange";
+  },
+  hghlght: function (ev){
+    ev.target.classList.toggle("highlight");
   }
 }
